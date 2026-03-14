6b0ef4a08a21f9224554e4fe20b9515194349b41
Priyanshu Kumar <kupriyanshu1212@gmail.com>
2026-03-14 13:36:28 +0530

    fix render route

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,9 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.get("/", (req, res) => {
+  res.send("College Backend Server Running Successfully 🚀");
+});
 /* ---------- STUDENT DATABASE ---------- */
 
 const studentDB = mysql.createConnection({
