e1478d5347211b82838e316d5b2a53188c273903
Priyanshu Kumar <kupriyanshu1212@gmail.com>
2026-03-14 13:22:52 +0530

    fix the port

--- backend/server.js
+++ backend/server.js
@@ -91,7 +91,8 @@ app.post("/faculty-login",(req,res)=>{
 
 });
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000,()=>{
-  console.log("🚀 Server running on http://localhost:3000");
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
